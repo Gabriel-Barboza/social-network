590919ed7c765c1cb40344e0d4231f349cd77eb9
Gabriel <oliveira.barboza84@gmail.com>
2025-01-27 19:37:23 -0300

    bug fix

--- api/src/router/rotas/rotas.go
+++ api/src/router/rotas/rotas.go
@@ -1,10 +1,22 @@
 package rotas
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 type Rota struct {
 	Url        string
 	Metodo     string
 	Funcao     func(http.ResponseWriter, *http.Request)
 	Autenticar bool
+}
+
+func Configurar(r *mux.Router) *mux.Router {
+	rotas := rotasUsuarios
+	for _, rota := range rotas {
+		r.HandleFunc(rota.Url, rota.Funcao).Methods(rota.Metodo)
+	}
+	return r
 }
